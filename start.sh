dd7dd20fd9464db0d86fadcb68ec0301540e45ab
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-06-08 22:08:31 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Apnamovi/Happy.git /PaidRepo
+  git clone https://github.com/Apnamovi/Happy.git /Happy
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
   git clone $UPSTREAM_REPO /Happy
