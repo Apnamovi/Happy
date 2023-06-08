59c1bc34852799a8d308c5f65bfb25d483ad1d9f
Apnamovi <99548630+Apnamovi@users.noreply.github.com>
2023-06-08 21:39:15 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/Apnamovi/PaidRepo.git /PaidRepo
+  git clone https://github.com/Apnamovi/Happy.git /PaidRepo
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /PaidRepo
+  git clone $UPSTREAM_REPO /Happy
 fi
-cd /PaidRepo
+cd /Happy
 pip3 install -U -r requirements.txt
 echo "Starting JK DEVELOPER BoT...."
 python3 bot.py
